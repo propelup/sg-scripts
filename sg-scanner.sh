0b017b611edf3c25e88ae15e6bf88267a38c8853
Biswaranjan Sahoo <biswaranjan.sahoo.2@team.telstra.com>
2020-11-04 12:33:36 +0000

    Debug enabled

--- sg-scanner.sh
+++ sg-scanner.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-
+set -x
 echo "Starting Security Assessment Scanning"
 docker run --rm --net=host -v ${WORKSPACE}:/project -v /var/run/docker.sock:/var/run/docker.sock --name scancont -e SEC_BASE_PATH=$SEC_BASE_PATH -e CLAIR_ADDR=$CLAIR_ADDR sg-scanner
