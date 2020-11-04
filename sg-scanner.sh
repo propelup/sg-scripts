749cecadeae69192c2fd1c48029478afa77f4623
Biswaranjan Sahoo <biswaranjan.sahoo.2@team.telstra.com>
2020-11-04 12:31:43 +0000

    Mounted the current workspace dir to docker container

--- sg-scanner.sh
+++ sg-scanner.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 echo "Starting Security Assessment Scanning"
-docker run --rm --net=host -v /var/lib/jenkins/workspace/Second:/project -v /var/run/docker.sock:/var/run/docker.sock --name scancont -e SEC_BASE_PATH=$SEC_BASE_PATH -e CLAIR_ADDR=$CLAIR_ADDR sg-scanner
+docker run --rm --net=host -v ${WORKSPACE}:/project -v /var/run/docker.sock:/var/run/docker.sock --name scancont -e SEC_BASE_PATH=$SEC_BASE_PATH -e CLAIR_ADDR=$CLAIR_ADDR sg-scanner
